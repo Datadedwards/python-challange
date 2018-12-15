5455131d9f7e5648b1cffa031f874fa8307ca225
Datadedwards <dedwards.data@gmail.com>
2018-12-15 12:47:15 -0500

    First commit

--- PyBank/David_Edwards_main.py
+++ PyBank/David_Edwards_main.py
@@ -8,6 +8,7 @@ total = 0
 change = 0
 minvalue = 0
 maxvalue = 0
+average = 0
 
 
 with open(budget_df, newline="") as csvfile:
@@ -20,7 +21,6 @@ with open(budget_df, newline="") as csvfile:
 with open(budget_df, newline="") as csvfile:
     csv_header = next(csvfile)
     
-
     for row in csv.reader(csvfile):
         total += int(row[1])
         change -= int(row[1])
@@ -34,11 +34,17 @@ with open(budget_df, newline="") as csvfile:
         minvalue = min(minvalue, value)
         maxvalue = max(maxvalue, value)
 
+with open(budget_df, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    
+    average = int(total/month_count)
         
 print("Financial Analysis")
 print("--------------------")       
 print(f"Total Months: {month_count}")       
 print(f"Total: ${total}")
+print(f"Average per month: ${average}")
 print(f"Greatest Increase in Profits: " + "${:}".format(maxvalue))
 print("Greatest Decrease in Profits: " + "${:}".format(minvalue))
     
@@ -51,6 +57,8 @@ file.write("\n")
 file.write(f"Total Months: {month_count}")
 file.write("\n")
 file.write(f"Total: ${total}")
+file.write("\n")
+file.write(f"Average per month: ${average}")
 file.write("\n")
 file.write("Greatest Increase in Profits: " + "${:}".format(maxvalue) +")")
 file.write("\n")
